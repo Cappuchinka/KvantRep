7bcf9f654a7f85011f2845cf59032d7780346310
Cappuchinka <volchenko.coffee@gmail.com>
2022-12-17 15:07:17 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def sum_matrix(matr1, matr2):
+    for i in range(len(matr1)):
+        for j in range(len(matr1[i])):
+            matr1[i][j] += matr2[i][j]
+    return matr1
+
+if __name__ == '__main__':
+    _matr1 = [[1, 2],
+              [3, 4]]
+
+    _matr2 = [[8, 7],
+              [6, 5]]
+
+    print(sum_matrix(_matr1, _matr2))
